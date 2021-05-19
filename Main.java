dfdae5f45ebb2ace8d492f40382867a4b2ff0ffc
Stevie H <74920914+stevie-h@users.noreply.github.com>
2021-05-19 17:44:43 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,42 @@
+package com.example;
+
+// https://qaarchint.files.wordpress.com/2021/05/java_10.pdf
+
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+        //ex1
+        // A web server is software (and possibly additional required hardware) used to process
+        // requests from a client and send responses back to the requester.
+
+        //ex2
+        // HTTP is the common request-response protocol used to distribute hpermedia information,
+        // usually for communication between web servers and their clients (e.g. web browsers).
+
+        //ex3
+        // A. Using Spring framework can be used to code web applications in a Java platform.
+        // B. Yes, it's designed for Java-based applications.
+
+        //ex4
+        // start.spring.io is used to generate a demo project for Spring based on the user's specs.
+
+        //ex5
+        // A Controller class is used to handle the client requests and control the data flow - retrieving
+        // and updating the data if needed.
+
+        //ex7
+        // Rest API is an interface for application programming according to the Rest architecture, normally
+        // for web services, used for CRUD (create, read, update, delete).
+
+        //ex8
+        // Postman is a simple, convenient way to check the requests and responses between client and server.
+
+
+    }
+
+
+}
